55441d3b78592fea59c5b8816b0e41a526bb69d9
Nikhil Saraswat <79843717+nikhilsaraswat2605@users.noreply.github.com>
2022-05-06 18:10:41 +0530

    updated

--- Iron Man Jarvis AI system/Jarvis.py
+++ Iron Man Jarvis AI system/Jarvis.py
@@ -15,35 +15,38 @@ from wikipedia import exceptions
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 # print(voices)
-engine.setProperty('voice',voices[0].id)
+engine.setProperty('voice', voices[0].id)
 # print(voices[1].id)
+
 
 def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>=0 and hour<12:
+    if hour >= 0 and hour < 12:
         speak("Good Morning!")
-    elif hour>=12 and hour<18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon!")
     else:
         speak("Good evening!")
-    speak("I'm Jarvice...  sir please tell me how may I help you.")   
+    speak("I'm Jarvice...  sir please tell me how may I help you.")
+
 
 def takeCommand():
-    """It takes microphone input from user and returns a string.""" 
+    """It takes microphone input from user and returns a string."""
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listning...")
         r.pause_threshold = 0.8
         r.energy_threshold = 300
         audio = r.listen(source)
-        
+
     try:
         print("Recognising...")
-        query = r.recognize_google(audio, language = "en-in")
+        query = r.recognize_google(audio, language="en-in")
         print(f"You said : {query}\n ")
         speak(f"You said, {query}. ")
     except Exception as e:
@@ -51,6 +54,7 @@ def takeCommand():
         print("Say that again please...")
         return "None"
     return query
+
 
 def send_email(to, content):
     speak("Sending your email..., please wait...")
@@ -61,19 +65,20 @@ def send_email(to, content):
     server.sendmail("nikhilsaraswat2605@gmail.com", to, content)
     server.close()
 
+
 if __name__ == '__main__':
     wishMe()
     while True:
         query = takeCommand().lower()
-        if "jarvis stop" in query or "stop" in query:
+        if "jarvis stop" in query or "stop" in query or 'quit' in query:
             print("Quiting...")
             speak("I am Quiting... Thank you for using me!")
             break
-            
+
         elif "wikipedia" in query:
             try:
                 print("Searching Wikipedia...")
-                query.replace("wikipedia","")
+                query = query.replace("wikipedia", "")
                 result = wikipedia.summary(query, sentences=2)
                 print(result)
                 speak(f"According to Wkipedia {result} ")
@@ -90,17 +95,17 @@ if __name__ == '__main__':
         elif "open facebook" in query or "open fb" in query:
             webbrowser.open_new_tab("facebook.com")
 
-        elif "open music" in query or "play song" in query:
+        elif "play music" in query or "play song" in query:
             try:
                 music_dir = "D:\\music"
                 songs = os.listdir(music_dir)
                 print(songs)
-                song_number = random.randint(0,len(songs)-1)
-                os.startfile(os.path.join(music_dir,songs[song_number]))
+                song_number = random.randint(0, len(songs)-1)
+                os.startfile(os.path.join(music_dir, songs[song_number]))
             except Exception as e:
                 print(e)
                 speak(e)
-        
+
         elif "the time" in query:
             strtime = datetime.datetime.now().strftime("%H:%M:%S")
             print(strtime)
@@ -109,7 +114,7 @@ if __name__ == '__main__':
         elif "open code" in query:
             codepath = "C:\\Users\\Asus\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
             os.startfile(codepath)
-        
+
         elif "send email to nikhil" in query:
             try:
                 speak("What should I say...")
@@ -125,28 +130,30 @@ if __name__ == '__main__':
         elif "change voice" in query or "change the voice" in query:
             # print(engine.getProperty('voice'))
             # print(voices[0].id)
-            if engine.getProperty('voice')==voices[0].id:
-                engine.setProperty('voice',voices[1].id)
+            if engine.getProperty('voice') == voices[0].id:
+                engine.setProperty('voice', voices[1].id)
                 speak("Hello sir...I am Alexa")
             else:
-                engine.setProperty('voice',voices[0].id)
+                engine.setProperty('voice', voices[0].id)
                 speak("Hello sir...I am Jarvis")
-            speak("The voice changed...Now enjoy with my voice.")    
-            
-            
-            
-        # else:
-        #     my_results_list = []
-        #     print("Searching...")
-        #     for i in search(query,        # The query you want to run
-        #         tld = 'com',  # The top level domain
-        #         lang = 'en',  # The language
-        #         num = 10,     # Number of results per page
-        #         start = 0,    # First result to retrieve
-        #         stop = 10,  # Last result to retrieve
-        #         pause = 2.0,  # Lapse between HTTP requests
-        #        ):
-        #         print("Searching...")
-        #         my_results_list.append(i)
-        #         print(i)
-        #         speak(i)
+            speak("The voice changed...Now enjoy with my voice.")
+
+
+        elif "google" in query:
+            if query == "":
+                continue
+            query = query.replace("google","")
+            my_results_list = []
+            print("Searching on google...")
+            for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+                my_results_list.append(j)
+            for i in my_results_list:
+                print(i)
+            webbrowser.open("https://google.com/search?q=%s" % query)
+
+        elif "search on youtube" in query:
+            if query == "":
+                continue
+            query = query.replace("search on youtube","")
+            print("Searching on youtube...")
+            webbrowser.open("https://youtube.com/search?q=%s" % query)
